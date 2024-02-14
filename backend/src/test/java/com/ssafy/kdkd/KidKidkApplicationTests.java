90fda5f517ccd8cb1de178c10b7c3efa65cef016
고은석 <ko2sist@gmail.com>
2024-02-14 12:26:12 +0900

    release-be

--- backend/src/test/java/com/ssafy/kdkd/KidKidkApplicationTests.java
+++ backend/src/test/java/com/ssafy/kdkd/KidKidkApplicationTests.java
@@ -11,13 +11,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class KidKidkApplicationTests {
 
-	@Value("${jwt.secret}")
-	private String secretKey;
+	// @Value("${jwt.secret}")
+	// private String secretKey;
 
-	@Test
-	@DisplayName("시크릿 키 존재 여부 체크")
-	void test1() {
-		assertThat(secretKey).isNotNull();
-	}
+	// @Test
+	// @DisplayName("시크릿 키 존재 여부 체크")
+	// void test1() {
+	// 	assertThat(secretKey).isNotNull();
+	// }
 
 }
